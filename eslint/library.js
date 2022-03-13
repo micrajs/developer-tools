a9ae1df08956261a45ed94a2691d9b37ef3e956b
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-03-13 16:18:15 -0500

    feat: added eslint and commit-lint configs

---
+++ eslint/library.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+  },
+};
