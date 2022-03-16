f592e893b7125cc3cbf775c07170633b40299737
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-03-16 16:33:44 -0500

    feat: added eslint-plugin-prettier

--- typescript.js
+++ typescript.js
@@ -1,11 +1,13 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: ['plugin:@typescript-eslint/recommended'],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
   rules: {
+    'prettier/prettier': 'error',
     '@typescript-eslint/no-namespace': 'off',
     '@typescript-eslint/no-unused-vars': [
       'error',
